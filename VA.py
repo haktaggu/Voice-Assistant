5349036bc145933321f584b4dac17d9c92000dc7
haktaggu <haktaggu02@Gmail.com>
2025-02-22 10:34:25 -0800

    Add files via upload
    V1.0 - GUI with Buttons

--- VA.py
+++ VA.py
@@ -1,17 +1,25 @@
 
-# Header
+# Header VA.py (Voice Assistant)
 #    Imports
+        # Tkinter Package (used for GUI)
+        # Speech Recognition Package (used for Speech Recognition)
+        # PyAudio Package (Used for Audio)
 from tkinter import *
 from tkinter import ttk
 import speech_recognition as sr
+import PyAudio as pa
 
 # GUI Frame
+    #Using Tkinter
 root = Tk()
 frm = ttk.Frame(root, padding = 20)
 frm.grid()
 
 
 #GUI Buttons
+    # Speak Button (used for Speech to Text)
+    # Type Button (Gives alternative option to Type)
+    # Exit Button (Terminates Program)
 ttk.Button(frm, text = "Speak", command = root.destroy).grid(column=0, row=0)
 ttk.Button(frm, text = "Type", command = root.destroy).grid(column=0, row=1)
 ttk.Button(frm, text = "Exit", command = root.destroy).grid(column=0, row=2)
