3fc94cc9cdc72da0f67d3acc0db7b2e074f0eea5
Yura441 <139591214+Yura441@users.noreply.github.com>
2025-02-22 16:13:15 -0800

    Add files via upload
    Separated code into functions, made it more clear

--- main.py
+++ main.py
@@ -1,49 +1,122 @@
-import os
-# importing necessary functions from dotenv library
-from dotenv import load_dotenv, dotenv_values 
-# loading variables from .env file
-from google import genai
-# importing gemini AI
+
+# Header VA.py (Voice Assistant)
+#    Imports
+        # Tkinter Package (used for GUI)
+        # Speech Recognition Package (used for Speech Recognition)
+        # PyAudio Package (Used for Audio)
+        
+from tkinter import *
+from tkinter import ttk
 import speech_recognition as sr
+import pyaudio as pa
+import os
+import pyttsx3 
+from dotenv import load_dotenv, dotenv_values
+from google import genai
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-# obtain audio from the microphone
+# GUI Frame
+    #Using Tkinter
+root = Tk()
+frm = ttk.Frame(root, padding = 20)
+root.title("My Voice Assistant")
+root.configure(bg="grey")
+root.geometry(f"372x124")
+frm.grid()
+
+# Speech Recognition Functions
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+#r.energy_threshold = 3000
 
-recognized_phrase = None
-
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    recognized_phrase = r.recognize_google(audio)
-    print("Google Speech Recognition thinks you said " + recognized_phrase)
-except sr.UnknownValueError:
-    recognized_phrase = None
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    recognized_phrase = None
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-load_dotenv() 
-
-# accessing gemini API key
+#Gemini Set Up
+load_dotenv()
 GEMINI_KEY = os.getenv("GEMINI_API_KEY")
-
 if not GEMINI_KEY:
-    print("It seems that you don't have the .env file configured!")
+    print("Error: Trouble accessing .env file!")
     raise Exception
 client = genai.Client(api_key=GEMINI_KEY)
 
+#TexttoSpeech Set Up
+
+speechEngine = pyttsx3.init()
+
+def textToSpeech(phrase):
+    speechEngine.say(phrase)
+    speechEngine.runAndWait()
+
+greetingPhrase = "Hello! I am your personal Voice Assistant. Click Speak to get started"
+
+textToSpeech(greetingPhrase)
+
+def open_browser(browserInput):
+        browser = webdriver.Firefox()
+        browser.get("https://google.com")
+        assert "Google" in browser.title
+        searchBox = browser.find_element(By.NAME, 'q') 
+        searchBox.send_keys(browserInput + Keys.RETURN)
+
+def generate_response(prompt):
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=prompt
+    )
+    return response.text
+
+def listen():
+    recognized_phrase = None
+    with sr.Microphone() as source:
+        #r.adjust_for_ambient_noise(source, duration=1)
+        print("Please talk...")
+        userSpeech = r.listen(source)
 
 
-input_text = recognized_phrase
+        try:
+            userSpeech = r.recognize_google(userSpeech)
+            print("Your Speech: " + userSpeech)
+            recognized_phrase = userSpeech
+            #label = ttk.Label(root, text = userSpeech)
+        
+        except sr.UnknownValueError:
+            recognized_phrase = None
+            print("I didn't understand that. Please try again?")
+            
+        except sr.RequestError as e:
+            recognized_phrase = None
+            print("Google Connection Error")
+    
+    return recognized_phrase
+
+def main():
+        
+        lowerCaseUserSpeech = listen().lower()
+        print(lowerCaseUserSpeech)
+
+        command = "gemini"
+        command_length = len(command)
+        if(lowerCaseUserSpeech[0:command_length])==command: 
+            input = lowerCaseUserSpeech[command_length+1:]
+            response = generate_response(input)
+            print(response)
+            textToSpeech(response)
+
+        command = "google"
+        command_length = len(command)
+        if(lowerCaseUserSpeech[0:command_length])==command: 
+            input = lowerCaseUserSpeech[command_length+1:]
+            open_browser(input)
+        
+        
+
+    
+#GUI Buttons
+    # Speak Button (used for Speech to Text)
+    # Type Button (Gives alternative option to Type)
+    # Exit Button (Terminates Program)
+ttk.Button(frm, text = "Speak", command = main).grid(column=0, row=0)
+ttk.Button(frm, text = "Type", command = root.destroy).grid(column=0, row=1)
+ttk.Button(frm, text = "Exit", command = root.destroy).grid(column=0, row=2)
 
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents=input_text
-)
-
-print(response.text)
+root.mainloop()
